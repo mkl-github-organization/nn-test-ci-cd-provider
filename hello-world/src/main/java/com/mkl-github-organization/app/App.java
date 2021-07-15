b237bdf2cc4e84f184163824ca4e39bff66ff6b8
nichtsnutz <nichtsnutz@users.noreply.github.com>
2021-07-15 18:14:47 +0200

    Add project hello-world after mvn archtype:generate

---
+++ hello-world/src/main/java/com/mkl-github-organization/app/App.java
@@ -0,0 +1,13 @@
+package com.mkl-github-organization.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
