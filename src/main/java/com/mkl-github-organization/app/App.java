a76c500bc322d0d802848b18efba61d80cc551d7
nichtsnutz <nichtsnutz@users.noreply.github.com>
2021-07-16 13:35:59 +0200

    Add javadoc comments to mitigate lint errors

--- src/main/java/com/mkl-github-organization/app/App.java
+++ src/main/java/com/mkl-github-organization/app/App.java
@@ -1,11 +1,23 @@
-/** java package names MUST NOT contain hyphons: http://java.sun.com/docs/books/jls/third_edition/html/packages.html **/
-package com.mkl_github_organization.app;
-
 /**
- * Hello world!
+ * Provides the classes App
+ * <p>
+ * The applet only outputs the string "Hello World!"
+ * An applet is an embeddable window (see the
+ * {@link java.awt.} class) with a few extra
+ * methods that the applet context can use to
+ * initialize, start, and stop the applet.
  *
+ * @since 1.0
+ * @see java.awt
  */
-public class App 
+package com.mkl_github_organization.app;
+/**
+ * java package names MUST NOT contain hyphons:
+ * http://java.sun.com/docs/books/jls/third_edition/html/packages.html
+**/
+
+
+public class App
 {
     public static void main( String[] args )
     {
--- src/test/java/com/mkl-github-organization/app/AppTest.java
+++ src/test/java/com/mkl-github-organization/app/AppTest.java
@@ -1,5 +1,20 @@
-/** java package names MUST NOT contain hyphons: http://java.sun.com/docs/books/jls/third_edition/html/packages.html **/
+/**
+ * Provides the classes App
+ * <p>
+ * The applet only outputs the string "Hello World!"
+ * An applet is an embeddable window (see the
+ * {@link java.awt.} class) with a few extra
+ * methods that the applet context can use to
+ * initialize, start, and stop the applet.
+ *
+ * @since 1.0
+ * @see java.awt
+ */
 package com.mkl_github_organization.app;
+/**
+ * java package names MUST NOT contain hyphons:
+ * http://java.sun.com/docs/books/jls/third_edition/html/packages.html
+**/
 
 import static org.junit.Assert.assertTrue;
 
@@ -8,7 +23,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
