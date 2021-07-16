e89f59b3c564a57103b857b38b0929208f12a29c
nichtsnutz <nichtsnutz@users.noreply.github.com>
2021-07-16 14:09:59 +0200

    Fix more lint errors

--- src/main/java/com/mkl-github-organization/app/App.java
+++ src/main/java/com/mkl-github-organization/app/App.java
@@ -1,7 +1,7 @@
 /**
- * Provides the classes App
+ * Provides the classes App.
  * <p>
- * The applet only outputs the string "Hello World!"
+ * The applet only outputs the string "Hello World!".
  * An applet is an embeddable window (see the
  * {@link java.awt.} class) with a few extra
  * methods that the applet context can use to
@@ -11,7 +11,7 @@
  * @see java.awt
  */
 package com.mkl_github_organization.app;
-/** 
+/**
  * java package names MUST NOT contain hyphons.
  * http://java.sun.com/docs/books/jls/third_edition/html/packages.html
 **/
@@ -22,7 +22,7 @@ public class App {
      * Outputs a single phrase.
      * @since 1.0
      **/
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
